ab6a53339deea5aa6c86530ac21c9fcb3a3c4e15
Rangarajan-R <46622213+Rangarajan-R@users.noreply.github.com>
2021-06-07 12:13:58 +0530

    Add files via upload

---
+++ SpringRestService/src/main/java/com/example/demo/User.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String gender;
+	private String country;
+	private String dateOfBirth;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String name, String gender, String country, String dateOfBirth) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.country = country;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", gender=" + gender + ", country=" + country + ", dateOfBirth="
+				+ dateOfBirth + "]";
+	}
+
+}
